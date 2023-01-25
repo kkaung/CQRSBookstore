16bb93826aedcdd7b84cd6639a3f4e32fc4c3e38
moonGoblin <kaungzaw1996@gmail.com>
2023-01-25 19:23:20 +1100

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CQRSBookStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
